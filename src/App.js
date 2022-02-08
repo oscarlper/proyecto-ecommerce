24f7714193345f544d3e47b1ba36bb713ea9340d
Oscar Pereyra <oscarlper@gmail.com>
2022-02-08 18:29:09 -0300

    inicio proyecto - navbar

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from 'react'
+//import logo from './logo.svg';
+import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import NavBar from './components/navbar'
+
+function App() {
+  return <>
+      {/* Complemente NavBar con bootstrap */}
+      <NavBar />
+      <h2>Las ofertas de la semana</h2>
+  </>;
+  ;
+}
+export default App;
+
