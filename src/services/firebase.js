28f8350fd7526d249f16402c7d54ba2c0a5e35ed
Oscar Pereyra <oscarlper@gmail.com>
2022-03-18 17:35:08 -0300

    ordenes en firebase

--- src/services/firebase.js
+++ src/services/firebase.js
@@ -1,6 +1,6 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-import { getDoc, getDocs, getFirestore, doc, collection, addDoc, Timestamp} from 'firebase/firestore';
+import { getFirestore, collection, addDoc, Timestamp} from 'firebase/firestore';
 
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
@@ -16,16 +16,6 @@ const firebaseConfig = {
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const db = getFirestore(app);
-
-export const getItems = async () => {
-    const items = await getDocs(collection(db, "productos"))
-    return items
-}
-
-export const getItem = async (id) => {
-    const item = await getDoc(doc(db, "items", id))
-    return item
-}
 
 export const generateOrder = async (order) => {
     const newOrder = addDoc(collection(db,"orders"), {
